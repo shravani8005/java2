1f44879d2c2bf2cdb4bad4e5581dc91ae43676a5
shravani8005/ <shravani8005@gmail.com>
2025-04-09 15:22:11 +0530

    main

---
+++ add.java
@@ -0,0 +1,14 @@
+package java2;
+
+public class add {
+ public static void main(String[] args) {
+        
+    
+        int a,b;
+        a=10; 
+        b=20;
+        int sum =a+b;
+        System.out.println("addition is " + sum);
+        
+    }
+}
