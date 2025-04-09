b01aab1db52a6c6a59abfdf9d0ef7024eb922482
shravani8005/ <shravani8005@gmail.com>
2025-04-09 15:24:38 +0530

    master

---
+++ subt.java
@@ -0,0 +1,14 @@
+package java2;
+
+public class subt {
+ public static void main(String[] args) {
+        
+    
+        int a,b;
+        a=100; 
+        b=20;
+        int subtract =a+b;
+        System.out.println("addition is " + subtract);
+        
+    }
+}
